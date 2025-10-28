3e82dd0316cf906b11e6cfd51e98039f398d0c86
ojpaak169@gmail.com <ojpaak169@gmail.com>
2025-10-28 07:52:29 +0300

    Commit 2
    In the new version on line 4, I changed object two's age from 25 to 30

--- IIB/src/Main.java
+++ IIB/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Student student1 = new Student("Kalamazi Nicholas",22);
-        Student student2 = new Student("Mujuzi Joram",25);
+        Student student2 = new Student("Mujuzi Joram",30);
         Student student3 = new Student("Matovu John",23);
 
     }
