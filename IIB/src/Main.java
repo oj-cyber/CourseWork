a52fff98605016bdb40156227cb1e45cc3c0b82b
ojpaak169@gmail.com <ojpaak169@gmail.com>
2025-10-28 07:42:08 +0300

    Commit 1
    In the new version on line 2,I created the main method and inside i created 3 student objects

---
+++ IIB/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student("Kalamazi Nicholas",22);
+        Student student2 = new Student("Mujuzi Joram",25);
+        Student student3 = new Student("Matovu John",23);
+
+    }
+}
