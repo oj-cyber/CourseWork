c9ace20cb11417a4e01c5b98f837e127f66a031c
ojpaak169@gmail.com <ojpaak169@gmail.com>
2025-10-28 07:27:59 +0300

    Commit 1
    In the new version on line 13,I created a constructor
    
    On line 19-32,I created getter method

--- IIB/src/Student.java
+++ IIB/src/Student.java
@@ -10,4 +10,25 @@ this.Id=nextId++;
         System.out.println("Student Id :" + Id + " assigned");
     }
 
+    public Student(String name, int id, int age) {
+        this.name = name;
+        Id = id;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
 }
